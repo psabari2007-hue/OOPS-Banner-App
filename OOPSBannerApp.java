aa0e4b27007543604d8f5ecd4a754d2e161125df
psabari2007-hue <psabari2007@gmail.com>
2026-03-03 09:21:56 +0530

    Implement ASCII banner display in main method
    Updated the main method to print a banner using ASCII art.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,21 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        
+        
+        String[] bannerLines = {
+            String.join("  ", " OOO ", " OOO ", "PPPP ", "SSS  "),
+            String.join("  ", "O   O", "O   O", "P   P", "S    "),
+            String.join("  ", "O   O", "O   O", "P   P", "S    "),
+            String.join("  ", "O   O", "O   O", "PPPP ", " SS  "),
+            String.join("  ", "O   O", "O   O", "P    ", "   S "),
+            String.join("  ", "O   O", "O   O", "P    ", "   S "),
+            String.join("  ", " OOO ", " OOO ", "P    ", "SSS  ")
+        };
+        
+        
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+        
     }
 }
